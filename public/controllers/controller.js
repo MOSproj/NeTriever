170bbbeb9308f6c4242303203362f31f6bbf7513
ohadc <ohadc.me@gmail.com>
2017-01-28 00:21:31 +0200

    And that's how it all begin

---
+++ public/controllers/controller.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("Hello World from controller");
+
+    $http.get('/posts').then(function (response) {
+        console.log(response);
+        $scope.posts = response;
+    });
+}]);
--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('test', ['categories', 'groups', 'posts']);
 
 app.use(express.static(__dirname + '/public'));
+
+db.on('error', function (err) {
+    console.log('database error', err)
+});
+
+db.on('connect', function () {
+    console.log('database connected')
+});
+
+app.get('/posts', function (req, res) {
+    db.posts.find({}, function (err, docs) {
+        console.log(docs)
+        res.json(docs);
+    });
+});
 
 app.listen(3000);
