a7da41d8b01c34f2280c1eead866bfc80f52d927
ohadc <ohadc.me@gmail.com>
2017-01-28 16:57:57 +0200

    bugs everywhere

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,9 +1,17 @@
 var myApp = angular.module('myApp', []);
-myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
-    console.log("Hello World from controller");
 
-    $http.get('/posts').then(function (response) {
+myApp.controller('categoriesCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.category = "asd";
+    $http.get('/categories').then(function (response) {
         console.log(response);
-        $scope.posts = response;
+        $scope.categories = response.data;
+    });
+}]);
+
+myApp.controller('postsCtrl', ['$scope', '$http', function ($scope, $http) {
+    console.log($scope.category);
+    $http.get('/posts' , $scope.category).then(function (response) {
+        console.log(response);
+        $scope.posts = response.data;
     });
 }]);
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
+// TODO create and use user with restrictions
 var db = mongojs('test', ['categories', 'groups', 'posts']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 db.on('error', function (err) {
     console.log('database error', err)
@@ -13,11 +16,34 @@ db.on('connect', function () {
     console.log('database connected')
 });
 
-app.get('/posts', function (req, res) {
+app.get('/categories', function (req, res) {
+    db.categories.find({}, function (err, docs) {
+        console.log(docs);
+        res.json(docs);
+    });
+});
+
+/*app.get('/posts', function (req, res) {
     db.posts.find({}, function (err, docs) {
         console.log(docs);
         res.json(docs);
     });
+});*/
+
+app.get('/posts', function (req, res) {
+    var category = req.query.category;
+    console.log(category);
+    console.log(category);
+    if(category == "") {
+        db.posts.find({}, function (err, docs) {
+            console.log(docs);
+            res.json(docs);
+        });
+    } else {
+        db.groups.find({'category': category}, function (err, docs) {
+            console.log(docs);
+        });
+    }
 });
 
 app.listen(3000);
