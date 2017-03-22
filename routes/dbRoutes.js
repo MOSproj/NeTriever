beb81d3b24dcaab0ec0ff67361c73328c3fbb703
Ohad Cohen <skillr1@gmail.com>
2017-03-22 13:26:31 +0200

    GET /get-posts/:category finally works

---
+++ routes/dbRoutes.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var config = require('../config');
+
+var dbFullPath = '';
+if(config.username != '')
+    dbFullPath += config.username + ':' + config.password + '@';
+if(config.dbPath != '')
+    dbFullPath += config.dbPath + '/';
+dbFullPath += config.dbName;
+
+var db = mongojs(dbFullPath, ['categories', 'groups', 'posts']);
+db.on('error', function (err) {
+    console.log('database error', err)
+});
+db.on('connect', function () {
+    console.log('database connected')
+});
+
+var getCategoriesName =  function (res) {
+    db.categories.find({}, {"name":1,_id:0}, function (err, docs) {
+        res(docs);
+    });
+};
+
+var getCategoryByName = function (categoryName, res) {
+    db.categories.findOne({'name': categoryName}, function (err, docs) {
+        res(docs);
+    });
+};
+
+var getGroupsByCategory = function (category, res) {
+    db.groups.find({'category.$id': category._id}, function (err, docs) {
+        res(docs);
+    });
+};
+
+var getPostsByGroups = function (groups, res) {
+    var groupsIds = [];
+    groups.forEach(function(group) {
+        groupsIds.push(group.id);
+    });
+    db.posts.find({'group_id': {'$in': groupsIds}}, function (err, docs) {
+        res(docs);
+    });
+};
+
+router.get('/get-categories', function (req, res) {
+    getCategoriesName(function (categoriesName) {
+        res.json(categoriesName);
+    });
+});
+
+router.get('/get-posts/:category', function (req, res) {
+    var categoryName = req.params.category;
+    console.log(categoryName + " category");
+    getCategoryByName(categoryName, function (category) {
+        getGroupsByCategory(category, function (groups) {
+            getPostsByGroups(groups, function (posts) {
+                res.json(posts);
+            });
+        });
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,54 +1,16 @@
 var express = require('express');
-var app = express();
 var mongojs = require('mongojs');
-var config = require('./config');
 var bodyParser = require('body-parser');
 
-var dbFullPath = '';
-if(config.username != '')
-    dbFullPath += config.username + ':' + config.password + '@';
-if(config.dbPath != '')
-    dbFullPath += config.dbPath + '/';
-dbFullPath += config.dbName;
+var dbRoutes = require('./routes/dbRoutes');
 
-var db = mongojs(dbFullPath, ['categories', 'groups', 'posts']);
+var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-
-
-db.on('error', function (err) {
-    console.log('database error', err)
-});
-
-db.on('connect', function () {
-    console.log('database connected')
-});
-
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
 
-app.get('/get-categories', function (req, res) {
-    db.categories.find({}, function (err, docs) {
-        console.log(docs);
-        res.json(docs);
-    });
-});
-
-app.get('/get-posts', function (req, res) {
-    db.posts.find({}, function (err, docs) {
-        console.log(docs);
-        res.json(docs);
-    });
-});
-
-app.get('/get-posts/:category', function (req, res) {
-    var category = req.params.category;
-    console.log(category + " category");
-    console.log(req);
-    db.groups.find({'category': category}, function (err, docs) {
-        console.log(docs);
-    });
-});
+app.use('/', dbRoutes);
 
 /* Fixing angular routing */
 /* http://stackoverflow.com/questions/29741759/node-js-404-and-angular-url-refresh-conflict */
