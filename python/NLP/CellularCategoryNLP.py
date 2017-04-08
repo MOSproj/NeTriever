d4edf068094dc16806668d0143accfe14e18b405
sivancoh91 <sivancoh91@gmail.com>
2017-04-08 11:36:18 +0300

    NLP  changes 0.4

--- python/NLP/CellularCategoryNLP.py
+++ python/NLP/CellularCategoryNLP.py
@@ -15,17 +15,17 @@ class CellularCategoryNLP:
         specs = dict()
         split_message = message.split()
         for key, value in CellularCategoryNLP.specs['words_after_indicator'].items():
-            spec_by_value = CellularCategoryNLP.__extract_word_after_indicator(split_message, value)
+            spec_by_value = CellularCategoryNLP.__extract_word_indicator(split_message, value, "after")
             if spec_by_value is not None:
                 specs[key] = spec_by_value
         for key, value in CellularCategoryNLP.specs['words_before_indicator'].items():
-            spec_by_value = CellularCategoryNLP.__extract_word_before_indicator(split_message, value)
+            spec_by_value = CellularCategoryNLP.__extract_word_indicator(split_message, value, "before")
             if spec_by_value is not None:
                 specs[key] = spec_by_value
         return specs
 
     @staticmethod
-    def __extract_word_after_indicator(split_message, indicators_to_extract):
+    def __extract_word_indicator(split_message, indicators_to_extract, state):
         index = 0
         match = False
         for word in split_message:
@@ -37,24 +37,12 @@ class CellularCategoryNLP:
                 index += 1
             else:
                 break
-        if index != len(split_message):
-            return split_message[index + 1]
-
-    @staticmethod
-    def __extract_word_before_indicator(split_message, indicators_to_extract):
-        index = 0
-        match = False
-        for word in split_message:
-            for indicator in indicators_to_extract:
-                if word == indicator:
-                    match = True
-                    break
-            if not match:
-                index += 1
-            else:
-                break
-        if index != len(split_message):
-            return split_message[index - 1]
+        if state == "after":
+            if index != len(split_message):
+                return split_message[index + 1]
+        else:
+            if index != len(split_message):
+                return split_message[index - 1]
 
 
 if __name__ == '__main__':
